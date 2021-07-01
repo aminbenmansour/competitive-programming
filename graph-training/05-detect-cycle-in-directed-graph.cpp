87d5bf7648e51f2e54ae1089eb5b51fc3b5f41bc
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2021-07-01 11:42:24 +0100

    recursive method, see if directed graph is cyclic

---
+++ graph-training/05-detect-cycle-in-directed-graph.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                cin.tie(0); cout.tie(0); ios::sync_with_stdio(0);
+
+#define all(v)				((v).begin()), ((v).end())
+#define sz(v)				((int)((v).size()))
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define rep(i, v)		    for(int i=0;i<sz(v);++i)
+#define lp(i, n)		    for(int i=0; i<(int)(n); i++)
+
+int nodes, edges;
+vector<vector<int>> graph;
+vector<int> seen;
+
+enum status {PATH = 0, CYCLE = 1};
+
+/**
+ * recursive method
+ * */
+
+bool cyclic = false;
+
+void dfs(int i, bool &cyclic) {
+    if(seen[i]) {
+        cyclic = true; return;
+    } else {
+        seen[i] = true;
+        int adj = graph[i].size();
+        lp(j, adj) {
+            int node = graph[i][j];
+            dfs(node, cyclic);
+        }
+        seen[i] = false;
+    }
+}
+
+/**
+ * end of the recursive method
+ * */
+
+
+int main(int argc, char **argv) {
+    fast;
+	cin >> nodes >> edges;
+
+    // count starts from zero !!
+    graph.resize(nodes); 
+    seen.resize(nodes); 
+
+    lp(i, edges) {
+        //direction is from node to adjacent
+        int node, adjacent; 
+        cin >> node >> adjacent;
+        graph[node].emplace_back(adjacent);
+    }
+ 
+    
+    status stat;
+    lp(i, nodes) {
+        dfs(i, cyclic);
+    }
+
+    string result = cyclic ? "cyclic" : "not cyclic";
+	cout << "the graph is " << result << '\n';
+    return 0 ;
+}
